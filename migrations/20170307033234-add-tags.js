44a082632c12472f9cd7d0fd79b93be105991ca5
Marvel Mathew <m@marvelmathew.ca>
2017-04-02 23:41:47 -0400

    Fix 'tags' migration for case when Sequelize creates the table on sync

--- migrations/20170307033234-add-tags.js
+++ migrations/20170307033234-add-tags.js
@@ -1,4 +1,13 @@
 module.exports = {
-  up: (qi, {Sequelize}) => qi.addColumn('pages', 'tags', Sequelize.STRING(1024)),
+  up: (qi, {Sequelize}) => {
+    return qi.describeTable('pages')
+      .then(table => {
+        // There is no better way to do CREATE TABLE IF NOT EXISTS
+        // See: https://github.com/sequelize/sequelize/issues/4934
+        if (!table['tags']) {
+          return qi.addColumn('pages', 'tags', Sequelize.STRING(1024))
+        }
+      })
+  },
   down: (qi, _Sequelize) => qi.removeColumn('pages', 'tags')
 }
