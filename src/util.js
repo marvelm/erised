a9cd15410ab5d0c42f73d26df061052c0409fd9d
Marvel Mathew <m@marvelmathew.ca>
2017-04-16 06:15:44 -0400

    Support env var 'ERISED_PATH'

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ const homedir = require('homedir')
 const path = require('path')
 
 const erisedDirectory = '.erised'
-const erisedPath = path.join(homedir(), erisedDirectory)
+const erisedPath = process.env.ERISED_PATH || path.join(homedir(), erisedDirectory)
 
 const archiveDirectory = 'archive'
 const archivePath = path.join(erisedPath, archiveDirectory)
